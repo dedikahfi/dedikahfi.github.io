db4c98e5e66e7c9a72e53918d0b2036675c77496
dedikahfi <dedikahfi@DESKTOP-49JO6A0>
2019-09-18 00:02:56 +0700

    navigation on class sidenav and topnav success

--- js/script.js
+++ js/script.js
@@ -1,4 +1,27 @@
 document.addEventListener("DOMContentLoaded", function() {
+	// Menampilkan drawer layaknya aplikasi native, dan memanggil nav dari nav.html
+	var elems = document.querySelector(".sidenav");
+	M.Sidenav.init(elems);
+	loadNav();
+
+	function loadNav() {
+		var xhttp = new XMLHttpRequest();
+		xhttp.onreadystatechange = function() {
+			if (this.readyState == 4){
+				if (this.status == 200){
+					// Muat daftar menu pada sidenav dan topnav
+					document.querySelectorAll(".topnav, .sidenav").forEach(function(elm) {
+						elm.innerHTML = xhttp.responseText;
+					});
+				} else {
+					return;
+				}
+			}
+		}
+		xhttp.open("GET", "nav.html", true);
+		xhttp.send();
+	}
+
 
 	// Navigasi untuk content
 	var page = window.location.hash.substr(1);
