1fe3aa3932525d1d5b1d2b0a44c04829c7e47364
dedikahfi <dedikahfi@DESKTOP-49JO6A0>
2019-09-17 23:28:12 +0700

    pwa add page home and pagination

---
+++ js/script.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+	// Navigasi untuk content
+	var page = window.location.hash.substr(1);
+	if (page == "") page = "home";
+	loadPage(page);
+
+	function loadPage(page) {
+		var xhttp = new XMLHttpRequest();
+		xhttp.onreadystatechange = function() {
+			if (this.readyState == 4) {
+				var content = document.querySelector("#body-content");
+				if (this.status == 200) {
+					content.innerHTML = xhttp.responseText ;
+				} else if (this.status == 404) {
+					content.innerHTML = "<p>Halaman tidak ditemukan.</p>";
+				} else {
+					content.innerHTML = "<p>Ups.. halaman tidak dapat diakses.</p>";
+				}
+			}
+		}
+		xhttp.open("GET", "pages/" + page + ".html", true);
+		xhttp.send();
+	}
+
+});
